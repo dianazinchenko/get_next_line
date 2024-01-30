66414c676f3faac92cb42ad3597b716f67f8a025
Diana Zinchenko <dzinchen@c1r15s13.42porto.com>
2024-01-30 11:28:35 +0000

    tested get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzinchen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 12:18:40 by dzinchen          #+#    #+#             */
+/*   Updated: 2023/11/11 15:18:18 by dzinchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_copy(char *new_string, char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_line(char *str);
+char	*ft_remove_prev_line(char *str);
+char	*ft_read_from_fd(char *str, int fd);
+char	*get_next_line(int fd);
+
+#endif
